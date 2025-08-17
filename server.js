aeb365009d079bcfd34509999ea17fede3801e23
ASahithi2005 <adlasahithi8@gmail.com>
2025-08-17 17:19:48 +0530

    change

--- server.js
+++ server.js
@@ -10,10 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB connection
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => console.log('MongoDB connected'))
+mongoose.connect(process.env.MONGO_URI).then(() => console.log('MongoDB connected'))
   .catch(err => console.error(err));
 
 // Import routes
