8a6231c757c7fb8cb3e3d4a1a7bb69076a7a532a
ASahithi2005 <adlasahithi8@gmail.com>
2025-08-17 15:51:05 +0530

    port changes

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.get('/', (req, res) => {
   res.send('API is running...');
   });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 
